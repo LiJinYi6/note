0b28ce2a7274df4348ebf2b7e1f0c31ac94bfb83
LiJinYi6 <3203345259@qq.com>
2024-11-06 23:25:29 +0800

    GGG

--- 构建工具笔记/webpack/nodejs/express/middle.js
+++ 构建工具笔记/webpack/nodejs/express/middle.js
@@ -5,11 +5,32 @@ const fc=function(req,res,next){
     next();
 }
 //全局生效的中间件
+app.use(express.static('public'))
 app.use(fc)
+//多次调用app.use可以注册多个中间件
 app.get('/home',(req,res)=>{
+    throw new Error("错误")
     res.send("hello")
 })
-
+app.use((err,req,res,next)=>{
+    console.log(err.message);
+    res.send(err.message)
+})
 app.listen(80,()=>{
     console.log("serve is start")
 })
+const ms=function(req,res,next){
+    console.log("这是一个局部生效的中间件")
+    next();
+}
+//局部生效的中间件
+app.get('/jmidle',ms,function(req,res){
+    res.send("局部中间件")
+})
+
+//内置中间件的使用
+app.post('/p',(req,res)=>{
+    res.send('ok')
+    console.log(req.body)
+})
+app.use(express.json)
